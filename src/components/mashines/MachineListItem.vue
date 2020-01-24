cadd3ead10176adffa79547a2492879386266225
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 19:10:17 +0200

    [master] MachineListItem component updated.

--- src/components/mashines/MachineListItem.vue
+++ src/components/mashines/MachineListItem.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="machine-list-item">
-    <p>{{ machine.name }}</p>
-  </div>
+  <router-link :to="routerLink" class="machine-list-item">
+    <p>{{ title }}</p>
+  </router-link>
 </template>
 
 <script>
   export default {
     name: 'machine-list-item',
     props: ['machine'],
+    computed: {
+      routerLink: function () {
+        return `/machine/${this.machine.id}`;
+      },
+      title: function () {
+        return this.machine.name.split('_').join(' ');
+      }
+    }
   }
 </script>
 
 <style scoped>
+  .machine-list-item {
+    color: #032036;
+    text-decoration: none;
+  }
 
+  .machine-list-item:hover {
+    color: #344974;
+    text-decoration: underline;
+  }
+
+  .machine-list-item:visited {
+    color: #032036;
+  }
+
+  .machine-list-item > p {
+    font: 500 1rem 'Avenir', Helvetica, Arial, sans-serif;
+    text-transform: capitalize;
+    text-align: left;
+  }
 </style>
 
 
