8557bc945bfed00abe4783bd04fcf2eedee4bff0
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 11:56:42 +0200

    [master] MachineListItem component created.

---
+++ src/components/mashines/MachineListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="machine-list-item">
+    <p>{{ machine.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'machine-list-item',
+  props: ['machine'],
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
