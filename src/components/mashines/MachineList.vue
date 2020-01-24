95e5a08f02c004de7a3ce62a8844ba81286c2b0f
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 18:05:03 +0200

    [master] machines query moved to separate file. MachineFragment created. Unnesessary .gql files deleted.

--- src/components/mashines/MachineList.vue
+++ src/components/mashines/MachineList.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="machine-list">
     <h2>Machine list component</h2>
-    <div>{{ hello }}</div>
+    <ApolloQuery :query="query"
+    >
+      <template slot-scope="{ result: { data, loading } }">
+        <div class="loader" v-if="loading">Loading...</div>
+        <ul v-if="data && data.machines">
+          <li v-bind:key="machine.id" v-for="machine of data.machines" class="machine-card">
+            {{ machine.name }}
+          </li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+import { ApolloQuery } from 'vue-apollo';
+import { machines } from '../../graphql/machines';
 
 export default {
-  apollo: {
-    hello: gql`query {
-      machines {
-        id
-      }
-    }`,
+  data () {
+    return {
+      query: machines,
+    }
+  },
+  components: {
+    ApolloQuery,
   },
   name: 'machine-list',
 }
---
+++ src/graphql/machines.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+import MachineFragment from './MachineFragment';
+
+export const machines = gql`
+    query machines {
+        machines {
+            ...MachineFragment
+        }
+    }
+    ${MachineFragment}
+`;
