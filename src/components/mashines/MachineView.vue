949c569899a10a06d768cbb7ba87a6e86826d961
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 19:55:41 +0200

    [master] ApolloQuery added to the MachineView component.

--- src/components/mashines/MachineView.vue
+++ src/components/mashines/MachineView.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="machine-view">
     <h3>Machine view</h3>
+    <ApolloQuery :query="query" :variables="{ id: $route.params.id }"
+    >
+      <template slot-scope="{ result: { data, loading } }">
+        <div class="loader" v-if="loading">Loading...</div>
+        <div v-if="data">
+          {{data}}
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
+  import { machine } from "../../graphql/machine";
+
   export default {
+    data() {
+      return {
+        query: machine,
+      }
+    },
     name: 'machine-view',
   }
 </script>
