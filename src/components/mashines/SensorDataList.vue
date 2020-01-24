eb7dca9c623f869d8cf5149d0493a57077c8b8cb
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 23:35:07 +0200

    [master] ApolloQuery added to the SensorDataList component.

--- src/components/mashines/SensorDataList.vue
+++ src/components/mashines/SensorDataList.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="sensor-data-list">
-    <p>{{selectedSensorId}}</p>
-
+    <ApolloQuery v-if="sensorId" :query="query" :variables="{ id: sensorId }"
+    >
+      <template slot-scope="{ result: { data, loading } }">
+        <div class="loader" v-if="loading">Loading...</div>
+        <div v-if="data">
+          <p>List on sensors data</p>
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -9,6 +16,11 @@
   import { sensor } from "../../graphql/sensor";
 
   export default {
+    data() {
+      return {
+        query: sensor,
+      }
+    },
     name: 'sensor-data-list',
     props: ['sensorId'],
   }
