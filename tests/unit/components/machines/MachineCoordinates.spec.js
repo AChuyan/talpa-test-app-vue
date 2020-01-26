a66090e62cfe83fb6f06a4a1ed191b331351806f
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-26 17:54:37 +0200

    [master] MachineCoordinates spec updated.

--- tests/unit/components/machines/MachineCoordinates.spec.js
+++ tests/unit/components/machines/MachineCoordinates.spec.js
@@ -2,15 +2,21 @@ import { shallowMount } from '@vue/test-utils'
 import MachineCoordinates from '@/components/machines/MachineCoordinates.vue'
 
 describe('MachineCoordinates component', () => {
-  test('is a Vue instance', () => {
-    const wrapper = shallowMount(MachineCoordinates);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallowMount(MachineCoordinates);
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  test('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   });
 
   test('renders correctly', () => {
-    const wrapper = shallowMount(MachineCoordinates);
-
     expect(wrapper.element).toMatchSnapshot()
   })
 });
